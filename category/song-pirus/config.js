6c1d346f7374dc6225bcb16b2fb048f834aacec3
hungsuy <rothlovesina@gmail.com>
2018-04-25 23:57:52 +0700

    Update

--- category/song-pirus/config.js
+++ category/song-pirus/config.js
@@ -112,14 +112,14 @@ function aplayer1 () {
             cover: 'https://dl.dropbox.com/s/pacp0vo2tkajao5/nekkrobvelea.jpg',
             theme: '#505d6b'
 			
-			},{
+		},{
             name: 'សម្រស់សោភា',
             artist: 'ស៊ីនស៊ីសាមុត',
             url: 'https://dl.dropbox.com/s/bdyi484wzhwhija/somros%20sophea.mp3',
             cover: 'https://dl.dropbox.com/s/pacp0vo2tkajao5/nekkrobvelea.jpg',
             theme: '#505d6b'
 			
-			},{
+		},{
             name: 'សូរិយាគងព្រៃ',
             artist: 'កែវ សារ៉ាត់',
             url: 'https://dl.dropbox.com/s/uh4tgyzbtf28fud/soriyakongprey.mp3',
@@ -135,26 +135,23 @@ function aplayer0 () {
         fixed: true,
         lrcType: 3,
         audio: [{
-            name: '前前前世',
-            artist: 'RADWIMPS',
-            url: 'https://moeplayer.b0.upaiyun.com/aplayer/yourname.mp3',
-            cover: 'https://moeplayer.b0.upaiyun.com/aplayer/yourname.jpg',
-            lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/yourname.lrc',
+            name: 'សូរិយាគងព្រៃ',
+            artist: 'កែវ សារ៉ាត់',
+            url: 'https://dl.dropbox.com/s/uh4tgyzbtf28fud/soriyakongprey.mp3',
+            cover: 'https://dl.dropbox.com/s/031btrjqw45gdy3/05-04-2018-800x445.jpg',
             theme: '#505d6b'
         }, {
-            name: 'トリカゴ',
-            artist: 'XX:me',
-            url: 'https://moeplayer.b0.upaiyun.com/aplayer/darling.mp3',
-            cover: 'https://moeplayer.b0.upaiyun.com/aplayer/darling.jpg',
-            lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/darling.lrc',
-            theme: '#46718b'
+            name: 'សម្រស់សោភា',
+            artist: 'ស៊ីនស៊ីសាមុត',
+            url: 'https://dl.dropbox.com/s/bdyi484wzhwhija/somros%20sophea.mp3',
+            cover: 'https://dl.dropbox.com/s/pacp0vo2tkajao5/nekkrobvelea.jpg',
+            theme: '#505d6b'
         }, {
-            name: '光るなら',
-            artist: 'Goose house',
-            url: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.mp3',
-            cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
-            lrc: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.lrc',
-            theme: '#ebd0c2'
+            name: 'ក្រោមមេឃលើដី',
+            artist: 'ស៊ីនស៊ីសាមុត',
+            url: 'https://dl.dropbox.com/s/kmn3xz5qrhin8au/krommeklerdey.mp3',
+            cover: 'https://dl.dropbox.com/s/pacp0vo2tkajao5/nekkrobvelea.jpg',
+            theme: '#505d6b'
         }]
     });
 }
