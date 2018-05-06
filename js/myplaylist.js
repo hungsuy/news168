acf7083ea3da281f2767e436f580c51a9fa82f11
hungsuy <rothlovesina@gmail.com>
2018-05-06 08:28:52 +0700

    hi

---
+++ js/myplaylist.js
@@ -0,0 +1,21 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'https://dl.dropbox.com/s/5v24baf6n2pi6y5/nekkrobvelea.mp3',
+        oga:'mix/1.ogg',
+        title:'Nek Krob Ve Lea',
+        artist:'Sin Si Samuth',
+        rating:4,
+        duration:'3:30',
+        cover:'mix/1.png'
+    }
+	
+];
