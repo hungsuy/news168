fa0e06e968108b24d0cd5279b83cb24db5cba9fe
hungsuy <rothlovesina@gmail.com>
2018-05-10 00:06:16 +0700

    Update

---
+++ src/js/musicList.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Smohan on 2015/5/15.
+ */
+    
+var musicList = [
+    {
+        title : 'នឹកគ្រប់វេលា',
+        singer : 'ស៊ីនស៊ីសាមុត',
+        cover  : 'https://dl.dropbox.com/s/y7aw89g1l3g7ova/Untitled-1.png',
+        src    : 'https://dl.dropbox.com/s/5v24baf6n2pi6y5/nekkrobvelea.mp3',
+		lyric  : "src/data/yqdmfsg.lrc"
+    },
+    {
+        title : 'ជីវិតមួយបេះដូងពីរ',
+        singer : 'ស៊ីនស៊ីសាមុត',
+        cover  : 'https://dl.dropbox.com/s/fl2kho5nj5sxcyc/Untitled-2.jpg',
+        src    : 'https://dl.dropbox.com/s/5n2ci3exggh9jnr/chivithmoury%20besdoung%20pi.mp3',
+		lyric  : "src/data/yqdmfsg.lrc"
+    },
+    {
+        title : 'កណ្ដាលដួងចិត្ត',
+        singer : 'ស៊ីនស៊ីសាមុត',
+        cover  : 'https://dl.dropbox.com/s/ci3z490mks4vad1/Untitled-3.jpg',
+        src    : 'https://dl.dropbox.com/s/hemc15zfqslo607/kandaldourngchet.mp3',
+		lyric  : "src/data/yqdmfsg.lrc"
+    },
+    {
+        title : 'ក្រោមមេឃលើដី',
+        singer : 'ស៊ីនស៊ីសាមុត',
+        cover  : 'https://dl.dropbox.com/s/7acqy695vakoeg6/Untitled-4.jpg',
+        src    : 'https://dl.dropbox.com/s/kmn3xz5qrhin8au/krommeklerdey.mp3',
+		lyric  : "src/data/yqdmfsg.lrc"
+    },
+    {
+        title : 'សម្រស់សោភា',
+        singer : 'ឈួយ សុភាព',
+        cover  : 'https://dl.dropbox.com/s/d82l0idtzzswlps/Untitled-5.jpg',
+        src    : 'https://dl.dropbox.com/s/bdyi484wzhwhija/somros%20sophea.mp3',
+		lyric  : "src/data/yqdmfsg.lrc"
+    },
+    {
+        title : 'សូរិយាគងព្រៃ',
+        singer : 'កែវ សារ៉ាត់',
+        cover  : 'https://dl.dropbox.com/s/4rl5j3anb84kkje/Untitled-6.jpg',
+        src    : 'https://dl.dropbox.com/s/uh4tgyzbtf28fud/soriyakongprey.mp3',
+		lyric  : "src/data/yqdmfsg.lrc"
+    }
+];
